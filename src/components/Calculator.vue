619a620be4ae5e8231f61267b02e47da2ab1d639
krava9999 <kosmo@internet.ru>
2021-09-20 13:09:25 +1000

    hw1

---
+++ src/components/Calculator.vue
@@ -0,0 +1,49 @@
+﻿<template>
+   <div>
+       <input v-model.number="operand1" type="number"/>
+       <input v-model.number="operand2" type="number"/> = {{result}}
+        <div class="keyboard">
+         <button @click="sum">+</button>
+         <button @click="minus">-</button>
+         <button @click="div">/</button>
+         <button @click="multi">*</button>
+          <button @click="int">int</button>
+         <button @click="intPow">**</button>
+     </div>
+
+   </div>
+</template>
+ 
+<script>
+ export default {
+   name: 'Calculator',
+   data(){
+       return {
+           operand1: 1,
+           operand2: 1,
+           result: 0,
+       }
+   },
+   methods: {
+     sum(){
+       return this.result = this.operand1 + this.operand2
+     },
+     minus(){
+      return this.result = this.operand1 - this.operand2
+     },
+     div(){
+      return this.result = this.operand1 / this.operand2
+     },
+     multi(){
+      return this.result = this.operand1 * this.operand2
+     },
+     int(){
+        return this.result = Math.floor((this.operand1/ this.operand2))
+      },
+     intPow(){
+       return this.result = this.operand1 ** this.operand2
+     }
+   },
+ 
+ }
+</script>
